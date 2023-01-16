6897ce3595a1101499ef6fda26ea4fda2a827aa5
grahamwwilson <gwwilson@ku.edu>
2023-01-16 08:55:20 -0600

    Now clroot.sh for executables requiring ROOT

--- cl.sh
+++ cl.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
-# You likely will need to adapt the CLI11.hpp include (it is provided) 
-# or install the CLI11 header file on your system. 
+# This needs the CLI11.hpp include (it is provided) or you can install the CLI11 
+# header file on your system. 
 # CLI11 provides a light-weight C++ method for command line argument parsing.
 #
-# Once you have that working, simply 
-# > ./cl.sh 
+# Simply 
+# ./cl.sh 
 # should be sufficient to make the Generate executable as this is the default.
 #
 
@@ -13,5 +13,6 @@ fn=${1:-Generate}
 
 # Compile with high optimization. No need for any libraries with this for now.
 g++ -O3 ${fn}.cpp -o ${fn}
+
 
 exit
